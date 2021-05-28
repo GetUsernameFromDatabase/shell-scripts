99c8c0ff8b70d4ede60db83f55cafa027568172a
Ryan Kruberg <49920260+GetUsernameFromDatabase@users.noreply.github.com>
2021-05-28 18:58:50 +0300

    Update manjaro.sh

--- linux/manjaro.sh
+++ linux/manjaro.sh
@@ -1,1 +1,23 @@
-pacman -S discord
+#!/bin/bash
+# Do this before "chmod +x ./manjaro.sh"
+set -eu -o pipefail # fail on error , debug all lines
+
+sudo -n true
+test $? -eq 0 || exit 1 "this script needs sudo priviledges"
+
+echo "installing pacman apps"
+pacman -Syu
+while read -r a ; do sudo pacman -S $a --noconfirm; done < <(cat << "EOF"
+    discord
+    fish
+    ttf-fira-code
+    torbrowser-launcher
+EOF
+)
+
+echo "installing snap apps"
+while read -r a ; do sudo snap install $a; done < <(cat << "EOF"
+    code --classic
+    teams
+EOF
+)

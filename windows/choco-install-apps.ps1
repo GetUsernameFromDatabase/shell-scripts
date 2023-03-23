72c6d1847f745099bed81b0c6fffd2aa985415e5
Ryan Kruberg <49920260+GetUsernameFromDatabase@users.noreply.github.com>
2023-03-23 22:10:10 +0200

    Update choco-install-apps.ps1

--- windows/choco-install-apps.ps1
+++ windows/choco-install-apps.ps1
@@ -7,50 +7,51 @@ ECHO "Installing apps via Chocolatey"
 choco feature enable -n allowGlobalConfirmation
 
 # MISC
-choco install shutup10 # "OOSU10" > win+R
-choco install defaultprogramseditor
+choco install chocolateygui
 choco install qbittorrent
 choco install autohotkey
+choco install wsl2
+
+# PERIPHERAL
+choco install lghub
+
+# WINDOWS
+choco install shutup10 # "OOSU10" > win+R
+choco install defaultprogramseditor
 
 # EDITORS
 choco install firacode # Font for editors
 choco install visualstudiocode
 choco install notepadplusplus # \/ makes it default editor
-cmd /c 'reg add "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\notepad.exe" /v "Debugger" /t REG_SZ /d "\"%ProgramFiles%\Notepad++\notepad++.exe\" -notepadStyleCmdline -z" /f'
 choco install 7zip
 
 # MEDIA
 choco install k-litecodecpackfull
 choco install paint.net
 choco install wincdemu
+choco install spotify
 
 # BROWSERS
 choco install firefox
 choco install tor-browser
-choco install microsoft-teams # big thonk
 
 # DEV
 choco install git
-choco install fiddler
 choco install gradle
-choco install python3
-choco install xampp-81
-## NODE
+choco install python
 choco install nvm
+choco install dotnet-sdk
+choco install openjdk
 
-# DEVOPS
+# DEV-OPS
 choco install sysinternals
 choco install putty
 choco install speccy
-choco install ccleaner
-choco install defraggler
-choco install filezilla
 #choco install ilspy 
-choco install malwarebytes
 
 # GAMING
 choco install geforce-experience
-choco install steam-client
+choco install steam
 choco install discord
 
 # Cleaning up

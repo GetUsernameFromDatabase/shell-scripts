cc66342dbd87cf708181daa0b2159479dcb4ce62
Ryan Murulo <49920260+GetUsernameFromDatabase@users.noreply.github.com>
2025-05-09 15:08:28 +0300

    feat: remove packages that update by themselves

--- windows/choco-install-apps.ps1
+++ windows/choco-install-apps.ps1
@@ -1,6 +1,9 @@
 # If chocolatey not installed
 ## Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
+# `choco uninstall xxx --skip-powershell --skip-autouninstaller` is used to make choco no longer manage the application 
+# > Use case: to prevent managing software that automatically updates, alternative is to pin
+
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) 
 { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
 ECHO "Installing apps via Chocolatey"
@@ -13,6 +16,7 @@ choco install autohotkey
 
 # PERIPHERAL
 choco install lghub
+choco uninstall lghub --skip-powershell --skip-autouninstaller
 
 # WINDOWS
 choco install shutup10 # "OOSU10" > win+R
@@ -20,8 +24,9 @@ choco install defaultprogramseditor
 
 # EDITORS
 choco install firacode # Font for editors
-choco install visualstudiocode
-choco install notepadplusplus # \/ makes it default editor
+choco install vscode
+choco uninstall vscode --skip-powershell --skip-autouninstaller
+choco install notepadplusplus
 choco install 7zip
 
 # MEDIA
@@ -29,10 +34,11 @@ choco install k-litecodecpackfull
 choco install paint.net
 choco install wincdemu
 choco install spotify
+choco uninstall spotify --skip-powershell --skip-autouninstaller
 
 # BROWSERS
 choco install firefox
-choco install tor-browser
+choco uninstall firefox --skip-powershell --skip-autouninstaller
 
 # DEV
 choco install git
@@ -49,9 +55,12 @@ choco install speccy
 #choco install ilspy 
 
 # GAMING
-choco install geforce-experience
+choco install nvidia-app
+choco pin add -n nvidia-app # pinning this since geforce experience uninstall seemed off
 choco install steam
+choco uninstall steam --skip-powershell --skip-autouninstaller
 choco install discord
+choco uninstall discord --skip-powershell --skip-autouninstaller
 
 # Cleaning up
 choco feature disable -n allowGlobalConfirmation
